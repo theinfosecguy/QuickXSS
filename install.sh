24f01f9ae0a6c5fdd7c74866c7da98bc8af2e051
Infosec Guy <keshavaarav22@gmail.com>
2021-03-07 15:03:45 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,7 +23,8 @@ GO111MODULE=on go get -v github.com/hahwul/dalfox/v2
 printf "Installing gau..\n"
 GO111MODULE=on go get -u -v github.com/lc/gau
 printf "Setting up GF Patterns\n"
-mkdir .gf
+mkdir ~/.gf
 cp -r $GOPATH/src/github.com/tomnomnom/gf/examples ~/.gf
+cd ~
 git clone https://github.com/1ndianl33t/Gf-Patterns
 mv ~/Gf-Patterns/*.json ~/.gf
