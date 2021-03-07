3f88d68236e5ebcc7dca5d3c60ea38a8a6daf5e9
Infosec Guy <keshavaarav22@gmail.com>
2021-03-07 13:17:22 +0530

    Added Output (-o) Argument & Func

--- QuickXSS.sh
+++ QuickXSS.sh
@@ -36,7 +36,7 @@ contruct_mode(){
     echo "$domain" | waybackurls | tee "$domain".txt >/dev/null 2>&1;
     printf "URLS fetched using waybackurls & Stored in $blue$domain.txt$end"
     printf "\n\nFinding URLs for $domain using gau ....\n"
-    echo "$1" | gau | tee -a $1 >/dev/null 2>&1;
+    echo "$1" | gau | tee -a $domain.txt >/dev/null 2>&1;
     printf "URLS fetched using gau & appended in $blue$domain.txt$end \n\n"
 
     echo -e "\nFinding valid URLs for XSS using GF Patterns \n"
@@ -44,9 +44,14 @@ contruct_mode(){
     cat "$domain".txt | gf xss | sed 's/=.*/=/' | sed 's/URL: //' | tee "$domain"_temp_xss.txt >/dev/null 2>&1;
 
     sort "$domain"_temp_xss.txt | uniq | tee "$domain"_xss.txt >/dev/null 2>&1;
-    printf "XSS Vulnerable URL's added to $blue"$domain"_xss.txt$end"
+    printf "\nXSS Vulnerable URL's added to $blue"$domain"_xss.txt$end\n\n"
 }
 
+usage(){
+    printf "Usage Coming Soon\n"
+}
+
+# Handling user arguments
 while [ -n "$1" ]; do
 	case $1 in
 		-d|--domain)
@@ -73,13 +78,16 @@ while [ -n "$1" ]; do
 	shift
 done
 
+# Creating Dir and fetch urls for a domain
 contruct_mode "$domain"
 
-if [ -z "$out"]
+# Check if Results Argument is present or not.
+if [ -z "$out" ]
 then
-    $out="results.txt"
+    out="results.txt"
 fi
 
+# STart XSS Hunting by checking if Blind XSS payload is present or not.
 if [ -z "$blind" ]
 then
     echo "XSS Automation Started using Dalfox.."
@@ -89,4 +97,5 @@ else
     dalfox file "$domain"_xss.txt -b $blind -o $out
 fi
 
+# Final Result
 echo -e "XSS automation completed, Results stored in$blue $domain Directory $end"
