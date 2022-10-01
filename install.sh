0b5a041bfee611c7831ead053b742b0b6e69ed67
tigv2 <72796679+tigv2@users.noreply.github.com>
2022-10-02 00:43:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ set -e
 cyan="\e[0;36m"
 end="\e[0m"
 
+# Banner
 echo -e "$cyan
  ██████╗  ██╗   ██╗██╗ ██████╗██╗  ██╗    ██╗  ██╗███████╗███████╗
  ██╔═══██╗██║   ██║██║██╔════╝██║ ██╔╝    ╚██╗██╔╝██╔════╝██╔════╝
@@ -13,6 +14,7 @@ echo -e "$cyan
  ╚██████╔╝╚██████╔╝██║╚██████╗██║  ██╗    ██╔╝ ██╗███████║███████║
   ╚══▀▀═╝  ╚═════╝ ╚═╝ ╚═════╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚══════╝╚══════╝
 $end\n"
+printf "By theinfosecguy.."
 
 printf "Installing GF..\n"
 go install github.com/tomnomnom/gf@latest
@@ -22,10 +24,16 @@ printf "Installing Dalfox..\n"
 go install github.com/hahwul/dalfox/v2@latest
 printf "Installing gau..\n"
 go install github.com/lc/gau@latest
+
 printf "Setting up GF Patterns\n"
+# Create directory for gf-patterns
 mkdir ~/.gf
+# Copy example gf patterns to gf directory
 cp -r $GOPATH/src/github.com/tomnomnom/gf/examples ~/.gf
 cd ~
+
+#Install GF Patterns
 git clone https://github.com/1ndianl33t/Gf-Patterns
 mv ~/Gf-Patterns/*.json ~/.gf
+
 printf "Installation Completed Successfully."
